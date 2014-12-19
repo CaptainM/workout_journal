ebef479f47222569b73671a7a71392ca03dbb583
CaptainM <caroline.a.morgan@vanderbilt.edu>
2014-12-19 09:12:37 -0500

    changed the ENV key

--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -1,7 +1,7 @@
 class Youtube
 
 	
-	CLIENT = YouTubeIt::Client.new(:dev_key => ENV["GOOGLE_API"])
+	CLIENT = YouTubeIt::Client.new(:dev_key => ENV["GOOGLE_YOUTUBE_API"])
 	def self.search(parameter)
 		response = CLIENT.videos_by(query: parameter)
 		return response.videos.first.media_content.first.url
