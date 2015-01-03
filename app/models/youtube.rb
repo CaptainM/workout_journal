d9c665dc1e3041b211f8c98c659a4b1565a23a03
CaptainM <caroline.a.morgan@vanderbilt.edu>
2015-01-02 19:47:13 -0500

    parsing into youtube URL to make it https

--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -4,7 +4,7 @@ class Youtube
 	CLIENT = YouTubeIt::Client.new(:dev_key => ENV["GOOGLE_YOUTUBE_API"])
 	def self.search(parameter)
 		response = CLIENT.videos_by(query: parameter)
-		return response.videos.first.media_content.first.url
+		return response.videos.first.media_content.first.url.insert(4, 's')
 	end
 
 end
